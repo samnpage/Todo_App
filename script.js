fe55c5fde8d42dc385c19c7fd0cef5e9ee8605fe
Sam Page <samnpage96@gmail.com>
2024-04-11 17:18:26 -0400

    1. Added local storage functionality. 2. Created Edit, and Delete functionality for current task. 3. Updated close task form button to check if they have values or not before being prompted. 4. Split submit button code into 2 helper methods; 'reset()', and 'addOrUpdateTAsk()'.

--- script.js
+++ script.js
@@ -14,35 +14,12 @@ const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
 
 // This array will store all the tasks along with their associated data, including title, due date, and description.
-const taskData = [];
+const taskData = JSON.parse(localStorage.getItem("data")) || [];
 
 let currentTask = {};
 
-// Reveals the form modal when user clicks the "Add new Task" button.
-openTaskFormBtn.addEventListener("click", () => {
-    taskForm.classList.toggle("hidden")
-});
-
-// Reveals the confirmCloseDialog modal when user clicks the closeTaskFormBtn.
-closeTaskFormBtn.addEventListener("click", () => {
-    confirmCloseDialog.showModal();
-});
-
-// Closes the confirmCloseDialog modal when user clicks the cancel button.
-cancelBtn.addEventListener("click", () => {
-    confirmCloseDialog.close();
-});
-
-// Closes the confirmCloseDialog modal and hides the form when user clicks the discard button.
-discardBtn.addEventListener("click", () => {
-    confirmCloseDialog.close();
-    taskForm.classList.toggle("hidden");
-});
-
-
-taskForm.addEventListener("submit", (e) => {
-    // Prevents the browser from refreshing the page after submitting the form.
-    e.preventDefault();
+const addOrUpdateTask = () => {
+    addOrUpdateTaskBtn.innerText = "Add Task";
 
     // Determines whether the task being added to the taskData array already exists of not.
     const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
@@ -55,4 +32,108 @@ taskForm.addEventListener("submit", (e) => {
         date: dateInput.value,
         description: descriptionInput.value
     }
+
+    // Adds the taskObj to the beginning of the taskData array, if the array does not already exist.
+    if (dataArrIndex === -1) {
+        taskData.unshift(taskObj);
+    } else {
+        taskData[dataArrIndex] = taskObj;
+    }
+
+    localStorage.setItem("data", JSON.stringify(taskData));
+
+    updateTaskContainer();
+    reset();
+};
+
+const updateTaskContainer = () => {
+    tasksContainer.innerHTML = "";
+    taskData.forEach(({id, title, date, description}) => {
+        (tasksContainer.innerHTML += `
+            <div class="task" id="${id}">
+                <p><strong>Title:</strong> ${title}</p>
+                <p><strong>Date:</strong> ${date}</p>
+                <p><strong>Description:</strong> ${description}</p>
+                <button type="button" class="btn" onclick="editTask(this)">Edit</button>
+                <button type="button" class="btn" onclick="deleteTask(this)">Delete</button>
+            </div>
+            `)
+        }
+    );
+};
+
+const deleteTask = (buttonEl) => {
+    // Finds the index of the task the user wants to delete.
+    const dataArrIndex = taskData.findIndex((item) => item.id === buttonEl.parentElement.id);
+
+    buttonEl.parentElement.remove();
+    taskData.splice(dataArrIndex, 1);
+    localStorage.setItem("data", JSON.stringify(taskData));
+};
+
+const editTask = (buttonEl) => {
+    // Finds the index of the task to be edited.
+    const dataArrIndex = taskData.findIndex((item) => item.id === buttonEl.parentElement.id);
+
+    currentTask = taskData[dataArrIndex];
+
+    titleInput.value = currentTask.title;
+    dateInput.value = currentTask.date,
+    descriptionInput.value = currentTask.description;
+
+    addOrUpdateTaskBtn.innerText = "Update Task";
+
+    taskForm.classList.toggle("hidden");
+};
+
+// Resets the input fields.
+const reset = () => {
+    titleInput.value = "";
+    dateInput.value = "";
+    descriptionInput.value = "";
+
+    // Reveals the form and clears currentTask array with current tasks the user has inputed.
+    taskForm.classList.toggle("hidden");
+    currentTask = {};
+}
+
+if (taskData.length) {
+    updateTaskContainer();
+}
+
+// Reveals the form modal when user clicks the "Add new Task" button.
+openTaskFormBtn.addEventListener("click", () => {
+    taskForm.classList.toggle("hidden")
+});
+
+// Reveals the confirmCloseDialog modal when user clicks the closeTaskFormBtn.
+closeTaskFormBtn.addEventListener("click", () => {
+    const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
+
+    const formInputValuesUpdated = titleInput.value !== currentTask.title || dateInput.value !== currentTask.date || descriptionInput.value !== currentTask.description;
+
+    if (formInputsContainValues && formInputValuesUpdated) {
+        confirmCloseDialog.showModal();
+    } else {
+        reset();
+    }
+});
+
+// Closes the confirmCloseDialog modal when user clicks the cancel button.
+cancelBtn.addEventListener("click", () => {
+    confirmCloseDialog.close();
+});
+
+// Closes the confirmCloseDialog modal and hides the form when user clicks the discard button.
+discardBtn.addEventListener("click", () => {
+    confirmCloseDialog.close();
+    reset();
+});
+
+
+taskForm.addEventListener("submit", (e) => {
+    // Prevents the browser from refreshing the page after submitting the form.
+    e.preventDefault();
+
+    addOrUpdateTask();
 });
